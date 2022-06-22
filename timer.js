e0b7b3b01f205057af74d31b39edc7452cb8351f
gabi <gabijk1234row@gmail.com>
2022-06-22 19:20:12 -0400

    base do projeto

---
+++ timer.js
@@ -0,0 +1,11 @@
+var inicio = document.getElementById('tnum')
+var inter = document.getElementById('tinter')
+var pausa = document.getElementById('tsec')
+
+function iniciar(){
+    var inicio = Number(inicio.value)
+    var inter = Number(inter.value)
+    var pausa = Number(pausa.value)
+
+
+}
