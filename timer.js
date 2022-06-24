5d6bff07d26c3469ce1abee9aacc58a12a6ea63b
gabi <gabijk1234row@gmail.com>
2022-06-24 07:22:01 -0400

    arrumando o JS e CSS

--- timer.js
+++ timer.js
@@ -1,8 +1,13 @@
-var acao = document.getElementById('tnum')
-var pausa = document.getElementById('tinter')
-var sessoes = document.getElementById('tsec')
+var acao = document.getElementById('acao')
+var pausa = document.getElementById('pausa')
+var sessoes = document.getElementById('sessoes')
 
 var segundos
+
+
+var Som_ambiente = new Audio("./audio/Lord of the Rings Sound of The Shire.mp3")
+var volta = new Audio("./audio/audio_volta.mp3")
+var final = new Audio("./audio/audio_final.mp3")
 
 var ambiente = document.getElementById('ambiente')
 var pause = document.getElementById('pause')
@@ -21,6 +26,145 @@ function executar(){
    pause.style.setProperty('display', 'block', 'important')
 }
 // Função para iniciar a contagem
+document.getElementById('timer').style.setProperty('display', 'none', 'important')
 function iniciar() {
    ambiente.play()  // Tocar a música automáticamente
+   if (acao.value == 0) {//campos de ação, pausa e sessões estão preenchidos
+      window.alert("Adicione os minutos")
+      acao.focus()
+   } else if (pausa.value == 0) {
+      window.alert("Adicione a pausa")
+      pausa.focus()
+   } else if (sessoes.value == 0) {
+      window.alert("Adicione as sessões")
+      sessoes.focus()
+   } else {
+
+      
+      pause.style.setProperty('display', 'block', 'important')
+
+      localStorage.setItem('acao', String(acao.value))//convertendo p strings
+      localStorage.setItem('pausa', String(pausa.value))
+      localStorage.setItem('sessoes', String(sessoes.value))
+
+       /* e sconder inputs e botão iniciar e mostrar div do timer*/
+      document.getElementById('config').style.setProperty('display', 'none', 'important')
+      document.getElementById('timer').style.setProperty('display', 'block', 'important')
+//fazer até aqui commit
+      momentoAcao()
+
+   }
+
+}
+function momentoAcao() {
+
+   let sessoes_valor = localStorage.getItem('sessoes')//pega os valores convertidos em string
+   if (sessoes_valor != '1') {
+      document.getElementById('title_sessao').innerHTML = sessoes_valor + ' sessões restantes'
+   } else {
+      document.getElementById('title_sessao').innerHTML = sessoes_valor + ' sessão restante'
+   }
+
+   var titulo = document.getElementById('title')//titulo h3 'tempo'
+   titulo.innerHTML = "Tempo"
+   titulo.style.fontSize = '25pt'
+   titulo.style.fontWeight = 'bold'
+   titulo.style.setProperty('color', '#d7ce42', 'important')
+   titulo.style.margin = 'auto'
+   titulo.style.textAlign = 'center'
+
+   min = Number(localStorage.getItem('acao'))//converte a string p num.
+
+   min = min - 1 //p relogio fazer a contagem regressiva certa 
+   segundos = 59
+
+   document.getElementById('minutes_ok').innerHTML = min // add minutos no HTML
+   document.getElementById('seconds_ok').innerHTML = segundos // add os segundos no HTML
+
+   var min_interval = setInterval(minTimer, 60000)//milissegundos p dar 60 segundos
+   var seg_interval = setInterval(segTimer, 1000)// 1 segundo
+
+   function minTimer() {
+      min = min - 1
+      document.getElementById('minutes_ok').innerHTML = min
+   }
+
+   function segTimer() {
+      segundos = segundos - 1
+      document.getElementById('seconds_ok').innerHTML = segundos
+
+      if (segundos <= 0) {
+         if (min <= 0) {
+            clearInterval(min_interval)
+            clearInterval(seg_interval)
+
+            ambiente.pause()//pausa a musica ambiente
+
+            volta.play()// soa o alarme
+            
+            momentoPausa()//chama a função pausa se zerar os minutos e segundos
+
+         }
+         segundos = 60 // recomeça os 60 segundos até os minutos chegarem á 0.
+      }
+
+   }
+}
+function momentoPausa() {
+
+   ambiente.play()//volta musica ambiente
+   
+   let titulo = document.getElementById('title')
+   titulo.innerHTML = "PAUSA"
+   titulo.style.fontSize = '25pt'
+   titulo.style.fontWeight = 'bold'
+   titulo.style.setProperty('color', '#dc3545', 'important')
+   titulo.style.margin = 'auto'
+   titulo.style.textAlign = 'center'
+ //mesmo processo de cima de Tempo
+   min_pausa = Number(localStorage.getItem('pausa'))
+   min_pausa = min_pausa - 1
+   segundos = 59
+   document.getElementById('minutes_ok').innerHTML = min_pausa
+   document.getElementById('seconds_ok').innerHTML = segundos
+
+   var min_interval = setInterval(minTimer, 60000)
+
+   var seg_interval = setInterval(segTimer, 1000)
+   function minTimer() {
+      min_pausa = min_pausa - 1
+      document.getElementById('minutes_ok').innerHTML = min_pausa
+   }
+
+   function segTimer() {
+      segundos = segundos - 1
+      document.getElementById('seconds_ok').innerHTML = segundos
+
+      if (segundos <= 0) {  
+         if (min_pausa <= 0) {
+            ses = Number(localStorage.getItem('sessoes'))
+            ses = ses - 1
+            localStorage.setItem('sessoes', String(ses))
+            clearInterval(min_interval)
+            clearInterval(seg_interval)
+
+            if (ses <= 0) {
+               final.play()
+               localStorage.clear()
+
+               //esconde o timer 
+               document.getElementById('config').style.setProperty('display', 'none', 'important')
+               document.getElementById('timer').style.setProperty('display', 'none', 'important')
+               document.getElementById('fim').style.setProperty('display', 'block', 'important')//msg final
+            } else {
+               // Senão toca o audio volta
+               volta.play();
+
+               momentoAcao()
+            }
+         }
+         segundos = 60
+      }
+   }
+
 }
