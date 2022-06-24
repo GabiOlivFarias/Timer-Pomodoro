e10e3fb84adc190da648606bb68b6d21e699d66c
gabi <gabijk1234row@gmail.com>
2022-06-23 22:32:53 -0400

    aprimorando a estrutura e arrumando JS

--- timer.js
+++ timer.js
@@ -1,11 +1,26 @@
-var inicio = document.getElementById('tnum')
-var inter = document.getElementById('tinter')
-var pausa = document.getElementById('tsec')
+var acao = document.getElementById('tnum')
+var pausa = document.getElementById('tinter')
+var sessoes = document.getElementById('tsec')
 
-function iniciar(){
-    var inicio = Number(inicio.value)
-    var inter = Number(inter.value)
-    var pausa = Number(pausa.value)
+var segundos
 
+var ambiente = document.getElementById('ambiente')
+var pause = document.getElementById('pause')
+var play = document.getElementById('play')
 
+function pausar(){ //tirar o botão pause e colocar o play
+   ambiente.pause()
+   play.style.setProperty('display', 'block', 'important')
+   pause.style.setProperty('display', 'none', 'important')
+}
+
+// Função para tocar a musica tirar o botão play e colocar o pause
+function executar(){
+   ambiente.play()
+   play.style.setProperty('display', 'none', 'important')
+   pause.style.setProperty('display', 'block', 'important')
+}
+// Função para iniciar a contagem
+function iniciar() {
+   ambiente.play()  // Tocar a música automáticamente
 }
